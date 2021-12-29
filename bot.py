129bd99042374c6a0c59f6b8e684ceee8f33dd1e
Doankt <36061217+Doankt@users.noreply.github.com>
2021-12-29 13:47:53 -0800

    Implement choice system

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 # Discord imports
-import discord
+# import discord
 import discord.ext.commands as commands
 
 # .env imports
@@ -13,9 +13,13 @@ from tabulate import tabulate
 
 # User imports
 from fflogsoauth import FFLogsOAuth
+from timetools import ms_to_mmssms
+
+# Other imports
+import asyncio
 
 # Define Client and Bot
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='?')
 ff_client = FFLogsOAuth(os.getenv("client_id"), os.getenv("client_secret"))
 
 # Ready Message
@@ -27,7 +31,7 @@ async def on_ready():
     print('------')
 
 # Rankings Command
-@bot.command()
+@bot.command(aliases=['r'])
 async def rankings(ctx, log_link):
     # Parse the log link
     parsed_url = urlparse(log_link)
@@ -45,6 +49,11 @@ async def rankings(ctx, log_link):
     {
         reportData{
             report(code: "%s"){
+                fights{
+                    id,
+                    startTime,
+                    endTime
+                },
                 rankings
             }
         }
@@ -52,28 +61,73 @@ async def rankings(ctx, log_link):
     ''' % report_id
 
     res = ff_client.query(q)
+
+    # Check < 1 clears
+    if not res['data']['reportData']['report']['rankings']['data']:
+        await ctx.send('`No rankings found`')
+        return
+
+    # Build time dict
+    time_dict = {f['id'] : f['endTime'] - f['startTime'] for f in res['data']['reportData']['report']['fights']}
+    # Build rankings dict
+    rankings_dict = {f['fightID'] : f for f in res['data']['reportData']['report']['rankings']['data']}
+
+    # Prompt user to select a fight
+    tmp_table = []
+    for fight in res['data']['reportData']['report']['rankings']['data']:
+        tmp_table.append([fight['fightID'], fight['encounter']['name'], ms_to_mmssms(time_dict[fight['fightID']])])
+    
+    pmt_msg = await ctx.send("`Enter the fight number you want to see rankings for\n"
+        + tabulate(tmp_table, headers=['Fight Number', 'Encounter', 'Time'], tablefmt='github')
+        + "`"
+    )
+
+    # Validate user input
+    def response_check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    try:
+        msg = await bot.wait_for('message', check=response_check, timeout=30.0)
+        i = int(msg.content)
+        
+        # Select the fight
+        fight = rankings_dict[i]
+        
+    except (IndexError, KeyError):
+        await ctx.send("`Invalid fight number`")
+        await msg.delete()
+        return
+    except asyncio.TimeoutError:
+        await ctx.send('`Request timed out`')
+        return
+    except ValueError:
+        await ctx.send('`Invalid input`')
+        await msg.delete()
+        return
+    finally:
+        await pmt_msg.delete()
+    await msg.delete()
+    
     
     # Format and send
-    for fight in res['data']['reportData']['report']['rankings']['data']:
-        if not fight['kill']: continue
-        tfight = []
-
-        for role in fight['roles'].values():
-            for player in role['characters']:
-                if 'server' not in player:    continue
-                
-                player_para = [
-                    player['name'],
-                    player['rank'],
-                    player['rankPercent']
-                ]
-                tfight.append(player_para)
-        
-        await ctx.send(
-            "`{encounter_name}\n{tbl}`\n".format(
-                encounter_name = fight['encounter']['name'],
-                tbl = tabulate(tfight, headers=['Name', 'Rank', 'Rank %'], tablefmt='github')
-            )
+    tfight = []
+    for role in fight['roles'].values():
+        for player in role['characters']:
+            if 'server' not in player:    continue
+            
+            player_para = [
+                player['name'],
+                player['rank'],
+                player['rankPercent']
+            ]
+            tfight.append(player_para)
+    
+    await ctx.send(
+        "`{encounter_name} - {fight_time}\n{tbl}`\n".format(
+            encounter_name = fight['encounter']['name'],
+            tbl = tabulate(tfight, headers=['Name', 'Rank', 'Rank %'], tablefmt='github'),
+            fight_time = ms_to_mmssms(time_dict[i])
         )
+    )
 
 bot.run(os.environ.get("discord_token"))
